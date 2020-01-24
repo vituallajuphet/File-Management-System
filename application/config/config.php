d5f6f7cf86f6d0e34a8c1ccf16ad6bbcf4103cec
juphet <jvitualla@proweaver.net>
2020-01-24 18:01:38 +0800

    Done uploading to local - 1-24-20

--- application/config/config.php
+++ application/config/config.php
@@ -34,7 +34,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://cbmcgroup.com.ph/docmanage/';
+$config['base_url'] = 'http://localhost/docmanage/';
 
 /*
 |--------------------------------------------------------------------------
